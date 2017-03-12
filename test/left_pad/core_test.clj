ffb1cc42152aa86e21a93694004bd52124b9c509
Sreejith Pp <mailz4sreejith@gmail.com>
2017-03-12 10:10:21 +0530

    Added tests

--- test/left_pad/core_test.clj
+++ test/left_pad/core_test.clj
@@ -3,5 +3,9 @@
             [left-pad.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Testing different arguments for leftpad"
+    (is (leftPad "hello" 9 "0") "0000hello")
+    (is (leftPad "hello" 8) "   hello")
+    (is (leftPad "hello" 1) "hello")
+    (is (leftPad "woah") "    woah")
+    ))
